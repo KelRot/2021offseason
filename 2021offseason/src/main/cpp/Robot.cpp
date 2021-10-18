93f89731c374bc1587413c8e4bf4486d50215c3c
KelRot <56571165+KelRot@users.noreply.github.com>
2021-10-18 17:31:59 +0300

    Update Robot.cpp

--- 2021offseason/src/main/cpp/Robot.cpp
+++ 2021offseason/src/main/cpp/Robot.cpp
@@ -1,6 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 #include "Robot.h"
 
@@ -17,12 +14,28 @@ double gyrokD;
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
   enc.SetDistancePerPulse((15*3.1415)/1024);
+  gyro.SetYawAxis(frc::ADIS16470_IMU::IMUAxis::kX);
 
   frc::CameraServer::GetInstance()->StartAutomaticCapture();
 
-  prefs= frc::Preferences::GetInstance();
+  frc::SmartDashboard::PutBoolean("Drive Reversed",false);
+
+}
+
+
+void Robot::RobotPeriodic() {
+   frc::SmartDashboard::PutNumber("Gyro Angle", gyro.GetAngle());
+   frc::SmartDashboard::PutNumber("Distance", enc.GetDistance());
+   frc::SmartDashboard::PutNumber("enc error", encpid.piderror);
+   std::cout<<gyro.GetAngle()<<std::endl;
+
+  
+
+  /*prefs= frc::Preferences::GetInstance();
   enckP= prefs->GetDouble("enckP",0.0);
   enckI= prefs->GetDouble("enckI",0.0);
   enckD= prefs->GetDouble("enckD",0.0);
@@ -31,62 +44,55 @@ void Robot::RobotInit() {
   gyrokI= prefs->GetDouble("gyrokI",0.0);
   gyrokD= prefs->GetDouble("gyrokD",0.0);
 
-  pid.kP=enckP;
-  pid.kI=enckI;
-  pid.kD=enckD;
+  encpid.kP=enckP;
+  encpid.kI=enckI;
+  encpid.kD=enckD;
 
   gyropid.kP=gyrokP;
   gyropid.kI=gyrokI;
-  gyropid.kD=gyrokD;
+  gyropid.kD=gyrokD;*/
+
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {
-   frc::SmartDashboard::PutNumber("Gyro Angle", gyro.GetAngle());
-   frc::SmartDashboard::PutNumber("Distance", enc.GetDistance());
-}
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
 void Robot::AutonomousInit() {
   m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
+ 
   std::cout << "Auto selected: " << m_autoSelected << std::endl;
   enc.Reset();
   gyro.Reset();
+  driveReversed=false;
 
 }
 
 void Robot::AutonomousPeriodic() {
-  drive.ArcadeDrive(pid.computePID(enc.GetDistance(),360,50),-gyropid.computePID(gyro.GetAngle(),0,10));
+  drive.ArcadeDrive(0,-gyropid.computePID(gyro.GetAngle(),90,5));
 }
 
 void Robot::TeleopInit() {
   enc.Reset();
   gyro.Reset();
+  driveReversed=false;//Default intake ön
 }
 
 void Robot::TeleopPeriodic() {
-  drive.CurvatureDrive(-js.GetRawAxis(1),js.GetRawAxis(4),js.GetRawButton(6));//butonlar ayarlanacak
-  if(js.GetRawButton(5)){
+  if(js.GetRawButtonPressed(4)){
+    driveReversed= !driveReversed; //Drive mode switched
+  }
+  if(driveReversed){
+    drive.CurvatureDrive(js.GetRawAxis(1),js.GetRawAxis(4),js.GetRawButton(5));
+    frc::SmartDashboard::PutBoolean("Drive Reversed",driveReversed);
+  }
+  else{
+    drive.CurvatureDrive(-js.GetRawAxis(1),js.GetRawAxis(4),js.GetRawButton(5));
+    frc::SmartDashboard::PutBoolean("Drive Reversed",driveReversed);
+  }
+
+  if(js.GetRawButton(6)){
     tirmanma.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, 0.5);
+  }
+  if(js.GetRawAxis(3)==1){
+    tirmanma.Set(ctre::phoenix::motorcontrol::ControlMode::PercentOutput, -0.5);
   }
 
 }

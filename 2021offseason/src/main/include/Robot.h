1a1e8cbd96adbaad5145f9076c088b2ec2dc10cb
KelRot <56571165+KelRot@users.noreply.github.com>
2021-10-19 09:06:10 +0300

    Update Robot.h

--- 2021offseason/src/main/include/Robot.h
+++ 2021offseason/src/main/include/Robot.h
@@ -12,52 +12,53 @@
 #include "Include.h"
 #include <ctre/Phoenix.h>
 class Robot : public frc::TimedRobot {
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
-  void TestInit() override;
-  void TestPeriodic() override;
+    public:
+        void RobotInit() override;
+        void RobotPeriodic() override;
+        void AutonomousInit() override;
+        void AutonomousPeriodic() override;
+        void TeleopInit() override;
+        void TeleopPeriodic() override;
+        void DisabledInit() override;
+        void DisabledPeriodic() override;
+        void TestInit() override;
+        void TestPeriodic() override;
 
-  frc::Joystick js{0};
+        frc::Joystick js{0};
 
-  frc::ADIS16470_IMU gyro{};
+        frc::ADIS16470_IMU gyro{};
 
-  frc::Talon solOn{0};
-  frc::Talon solArka{1};
-  frc::Talon sagOn{2};
-  frc::Talon sagArka{3};
-  frc::Encoder enc{0,1,true};
+        frc::Talon solOn{0};
+        frc::Talon solArka{1};
+        frc::Talon sagOn{2};
+        frc::Talon sagArka{3};
+        frc::Encoder enc{0,1,true};
 
-  frc::SpeedControllerGroup sol{solOn,solArka};
-  frc::SpeedControllerGroup sag{sagOn,sagArka};
+        frc::SpeedControllerGroup sol{solOn,solArka};
+        frc::SpeedControllerGroup sag{sagOn,sagArka};
 
-  frc::DifferentialDrive drive{sol,sag};
+        frc::DifferentialDrive drive{sol,sag};
 
-  PIDController encpid{0.047,0.0,0.0};
-  PIDController gyropid{0.027,0.0,0.24};
+        PIDController encpid{0.047,0.0,0.0};
+        PIDController gyropid{0.027,0.0,0.24};
 
-  VictorSPX tirmanma{2};
-  
-  bool driveReversed=false;
+        VictorSPX tirmanma{2};
+        
+        bool driveReversed=false;
 
-  double startPoint, ang, d, c; //baslangic noktasi, aci, mesafe, son mesafe
+        double startPoint, ang, d, c; //baslangic noktasi, aci, mesafe, son mesafe
 
-  const double donusSuresi = 1.0, gidisSuresi = 4.0;
-  //gidisK olarak degistirilecek
-  int phase = 0;
-  frc::Timer t;
+        const double donusSuresi = 1.0, gidisSuresi = 4.0;
+        //gidisK olarak degistirilecek
+        int phase = 0;
+        frc::Timer t;
 
-  double dist, angle;
+        double dist, angle;
 
- private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+    private:
+        frc::SendableChooser<std::string> m_chooser;
+        const std::string kAutoNameDefault = "Default";
+        const std::string kAutoNameCustom = "My Auto";
+        std::string m_autoSelected;
 };
+
